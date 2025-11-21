828daaac8fa80c9fa4042a2437dc6adb86220eaa
DiyaChandra07 <diyachandra807@gmail.com>
2025-11-21 19:16:16 +0000

    Declared functions for logs.c file

--- include/logs.h
+++ include/logs.h
@@ -3,7 +3,9 @@
 
 #include <stdio.h>
 
-/*Stucture to store one day's wellness data
+/*
+
+Stucture to store one day's wellness data
 
 fields: 
     day, month, year (date)
@@ -23,3 +25,19 @@ typedef struct DailyLog
     int waterGlasses;
 
 } ;
+
+/* 
+
+Declaring functionf for logs.c
+
+*/
+
+int add_daily_log(const char *filename, DailyLog *entry);
+DailyLog *read_all_logs(const char *filename, size_t *total_logs);
+DailyLog *find_logs_by_date (const char *filename, int d, int m, int y, size_t *total_logs);
+
+int date_equal (const DailyLog *a, int d, int m, int y);
+int split_date (const char *s, int *d, int *m, int *y);
+void print_log(const DailyLog *log);
+
+#endif
