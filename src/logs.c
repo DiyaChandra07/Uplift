10a65637b1d519265017cf870f63f258ae9337a0
DiyaChandra07 <diyachandra807@gmail.com>
2025-11-22 14:06:35 +0000

    read logs function updated

--- src/logs.c
+++ src/logs.c
@@ -4,7 +4,7 @@
 #include "../include/logs.h"
 
 
-
+//adding daily log 
 int add_daily_log(const char *filename, DailyLog *entry) 
 {
     FILE *f = fopen(filename, "a");
@@ -22,5 +22,38 @@ int add_daily_log(const char *filename, DailyLog *entry)
     
     fclose(f);
     return 1;
+}
+
+
+//Reading logs line by line
+DailyLog *read_all_logs(const char *filename, size_t *total_logs)
+{
+        total_logs = 0;
+
+        FILE *f = fopen(filename, "r");
+        if (f == NULL)
+        {
+            return NULL;
+        }
+
+        DailyLog *arr = NULL;
+        DailyLog temp;
+
+        //Reading until EOF
+        while (fscanf(f, "%d %d %d %d %f %d",
+                        &temp.day, &temp.month, &temp.year,
+                        &temp.mood,
+                        &temp.sleepHrs,
+                        &temp.waterGlasses)==6)
+        {
+            arr = realloc(arr,(*total_logs + 1) * sizeof(DailyLog));
+            arr[*total_logs] = temp;
+            (*total_logs)++;
+        }
+
+        fclose(f);
+        return arr;
+
+
 
 }
